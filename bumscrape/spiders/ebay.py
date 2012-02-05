03955807e7b545b1790af91c8ddd6204832052cd
Igor Serebryany <igor47@moomers.org>
2012-02-05 13:23:46 -0800

    paginate ebay search results; process results into item objects

--- bumscrape/spiders/ebay.py
+++ bumscrape/spiders/ebay.py
@@ -5,6 +5,7 @@ import re
 import urllib
 
 from scrapy.spider import BaseSpider
+from scrapy.http import Request
 from bumscrape.items import BumscrapeItem
 
 def get_query_url(endpoint, parameters):
@@ -13,6 +14,7 @@ def get_query_url(endpoint, parameters):
 class EbayScraper(BaseSpider):
     name = "ebay"
 
+    pageing = False
     endpoint = "http://svcs.ebay.com/services/search/FindingService/v1"
     parameters = {
             'OPERATION-NAME':'findItemsAdvanced',
@@ -22,10 +24,29 @@ class EbayScraper(BaseSpider):
             'REST-PAYLOAD':'1',
             'keywords':'burning man',
             'categoryId':1305,
+            'paginationInput.entriesPerPage':100,
+            'paginationInput.pageNumber':1,
             }
 
     start_urls = [get_query_url(endpoint, parameters)]
 
     def parse(self, http_response):
-        data = json.loads(http_response.body)
-        print data
+        data = json.loads(http_response.body)['findItemsAdvancedResponse'][0]
+
+        pagecount = int(data['paginationOutput'][0]['totalPages'][0])
+        if not self.pageing and pagecount > 1:
+            self.log("*** Going to make %s additional requests" % (pagecount,))
+
+            self.pageing = True
+            for pagenum in xrange(2, pagecount + 1):
+                self.parameters['paginationInput.pageNumber'] = pagenum
+                yield Request(get_query_url(self.endpoint, self.parameters), callback=self.parse)
+
+        for item in data['searchResult'][0]['item']:
+            yield BumscrapeItem(url=item['viewItemURL'][0],
+                                title=item['title'][0],
+                                price=item['sellingStatus'][0]['currentPrice'][0]['__value__'],
+                                #bids=item[0]['bidCount'],
+                                #created=item[0]['listingInfo'][0]['startTime'],
+                                #num_tickets=1, #correct number might be in an aspect...
+                                )
