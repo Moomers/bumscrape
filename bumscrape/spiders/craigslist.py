279f675eed9036b078203ddb16c1dad30633e7bc
Jered Wierzbicki <jeredw@gmail.com>
2012-02-06 01:05:28 -0600

    add post date

--- bumscrape/spiders/craigslist.py
+++ bumscrape/spiders/craigslist.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import datetime
 import re
 
 from scrapy.spider import BaseSpider
@@ -29,14 +30,11 @@ class CraigslistSpider(BaseSpider):
     # Matches a price with a dollar sign in front.
     price_re = re.compile(r"\$(\d+(?:\.\d+)?)")
 
-    # These urls are people looking for tickets, or 
-    wanted_url_re = re.compile(r".*/wan/|.*/clt/|.*/clo/")
-
-    # these strings inside the title indicate looking for tickets
-    wanted_titles = ['want', 'looking for', 'looking 4', 'requested', 'need']
-
-    # these strings in the title mean the post is about burning man
-    topical_titles = ['bm', 'burning man']
+    # Matches a craigslist posting date.
+    # We just grab the approximate post date from the search result page,
+    # because it's not worth it to crawl the result page just for a date.
+    date_re = re.compile(
+        r"^((?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+\d+)")
 
     def parse_result(self, result):
         """Parses a single result"""
@@ -45,10 +43,17 @@ class CraigslistSpider(BaseSpider):
 
         url = get_string("a/@href")
         title = get_string("a/text()")
-        match_price = self.price_re.search(get_string("text()"))
+        text = get_string("text()")
+        match_price = self.price_re.search(text)
         price = match_price.groups(1)[0] if match_price else None
+        match_date = self.date_re.search(text)
+        posted = None
+        if match_date:
+            date_string = match_date.groups(1)[0]
+            posted = datetime.datetime.strptime(
+                "%s 2012" % date_string, "%b %d %Y")
 
-        return title, url, price
+        return title, url, price, posted
 
     def parse(self, response):
         """Parses a list of results"""
@@ -56,5 +61,8 @@ class CraigslistSpider(BaseSpider):
         results = hxs.select("//p[@class = 'row']")
 
         for result in results:
-            title, url, price = self.parse_result(result)
-            yield BumscrapeItem(title=title, url=url, price=price)
+            title, url, price, posted = self.parse_result(result)
+            yield BumscrapeItem(title=title,
+                                url=url,
+                                price=price,
+                                posted=posted)
