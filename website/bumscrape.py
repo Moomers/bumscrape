42fba31ae4672377e1e8ad7809c8811fea815886
Igor Serebryany <igor47@moomers.org>
2012-02-07 15:47:09 -0800

    initial web framework for getting the listings

--- website/bumscrape.py
+++ website/bumscrape.py
@@ -1,11 +1,24 @@
 import web
+import model
 
+render = web.templates.render('templates/')
 urls = (
-      '/.*', 'hello',
+      '/', 'index',
+      '/list', 'list',
       )
 
-class hello:
+class index:
     def GET(self):
-       return "Hello, world."
+        raise web.redirect('/index.html')
+
+class list:
+    def GET(self):
+       """gets active listings"""
+       request = web.input(list_all=False, format='web')
+
+       active_only = True if not request.list_all else False
+       listings = model.get_listings(active_only)
+
+       return render.listings(listings)
 
 application = web.application(urls, globals()).wsgifunc()
