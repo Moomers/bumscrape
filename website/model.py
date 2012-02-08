ebbb81a1967543d8170ba124c2d377228928ee3c
Jered Wierzbicki <jeredw@gmail.com>
2012-02-07 21:48:16 -0600

    foo

--- website/model.py
+++ website/model.py
@@ -4,7 +4,7 @@ db = web.database(dbn='mysql', db='bumscrape', user="bumui", pw="iamthebumui")
 
 def get_listings(active_only = True):
     query = [
-        "SELECT url, l.spider, c.last_crawled, MAX(visited) AS last_seen, MIN(visited) AS first_seen, COUNT(*) AS visits, title, price, num_tickets, posted",
+        "SELECT url, l.spider, c.last_crawled, date_format(MAX(visited), '%c/%e') AS last_seen, date_format(MIN(visited), '%c/%e') AS first_seen, COUNT(*) AS visits, title, concat('$', format(c.price, 2)) AS price, num_tickets, posted",
         "FROM listings AS l JOIN crawls AS c ON l.spider = c.spider",
         "GROUP BY url"]
 
