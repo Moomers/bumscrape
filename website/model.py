9a8b183147f199b7f0cb75fb96bfc858aed474b1
Jered Wierzbicki <jeredw@gmail.com>
2012-02-07 22:08:05 -0600

    fffufuufu

--- website/model.py
+++ website/model.py
@@ -2,14 +2,11 @@ import web
 
 db = web.database(dbn='mysql', db='bumscrape', user="bumui", pw="iamthebumui")
 
-def get_listings(active_only = True):
+def get_listings():
     query = [
-        "SELECT url, l.spider, c.last_crawled, date_format(MAX(visited), '%c/%e') AS last_seen, date_format(MIN(visited), '%c/%e') AS first_seen, COUNT(*) AS visits, title, concat('$', format(price, 2)) AS price, num_tickets, posted",
+        "SELECT url, l.spider, c.last_crawled, max(visited) as last_seen, date_format(MAX(visited), '%c/%e') AS last_seen_fmt, min(visited) as first_seen, date_format(MIN(visited), '%c/%e') AS first_seen_fmt, COUNT(*) AS visits, title, concat('$', format(price, 2)) AS price, num_tickets, posted",
         "FROM listings AS l JOIN crawls AS c ON l.spider = c.spider",
         "GROUP BY url"]
-
-    if active_only:
-        query.append("HAVING last_seen >= last_crawled")
 
     query.append("ORDER BY spider DESC, first_seen DESC")
 
