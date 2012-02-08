5c50f0d66c5e53afe5915fde30426ae396526e11
Jered Wierzbicki <jeredw@gmail.com>
2012-02-07 21:30:44 -0600

    wtf

--- website/bumscrape.py
+++ website/bumscrape.py
@@ -14,12 +14,7 @@ import model
 render = web.template.render('templates/')
 urls = (
       '/', 'listings',
-      #'/about', 'about',
       )
-
-#class about:
-#    def GET(self):
-#        return render.about()
 
 class listings:
     def GET(self):
