2161138d4de77944b3e917dcf2f4468a048d894d
Jered Wierzbicki <jeredw@gmail.com>
2012-02-08 19:06:08 -0800

    json and csv

--- website/bumscrape.py
+++ website/bumscrape.py
@@ -10,6 +10,9 @@ os.chdir(abspath)
 
 import web
 import model
+import json
+import csv
+import StringIO
 
 render = web.template.render('templates/')
 urls = (
@@ -22,6 +25,7 @@ class about:
         return render.about()
 
 class listings:
+    fields = ['num_tickets', 'price', 'title', ''
     def GET(self):
        """gets active listings"""
        request = web.input(list_all=False, format='web')
@@ -29,6 +33,21 @@ class listings:
        active_only = True if not request.list_all else False
        listings = model.get_listings(active_only)
 
-       return render.listings(listings)
+       if format == 'json':
+           web.header('Content-Type', 'application/json')
+           return json.dumps({'listings': [
+               dict((self.fields[i], listing[i])
+                    for i in xrange(len(self.fields)))
+               for listing in listings]})
+       elif format == 'csv':
+           buf = StringIO.StringIO()
+           out = csv.writer(buf)
+           out.writerow(self.fields) 
+           for row in listings:
+               out.writerow(row)
+           web.header('Content-Type', 'application/csv')
+           return buf.getvalue()
+       else:
+           return render.listings(listings)
 
 application = web.application(urls, globals()).wsgifunc()
